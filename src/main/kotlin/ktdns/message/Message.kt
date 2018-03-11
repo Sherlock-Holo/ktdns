4b0704db6bbfdf6b16b4d4fb85a8757ed4228512
Sherlock Holo <sherlockya@gmail.com>
2018-03-11 10:27:09 +0800

    Rewrite Message.{Header, Questions}

--- src/main/kotlin/ktdns/message/Message.kt
+++ src/main/kotlin/ktdns/message/Message.kt
@@ -1,13 +1,33 @@
 package ktdns.message
 
+import java.nio.ByteBuffer
+
 class Message {
     var header = Header()
 
-    var question = Question()
+    val questions = ArrayList<Question>()
+
+    val answers = ArrayList<Answer>()
+
+    val byteArray: ByteArray
+        get() {
+
+            val arrayList = ArrayList<Byte>()
+
+            /** header **/
+            arrayList.addAll(header.byteArray.toTypedArray())
+
+            /** questions **/
+            questions.forEach {
+                arrayList.addAll(it.byteArray.toTypedArray())
+            }
+
+
+            return arrayList.toByteArray()
+        }
 
     class Header {
         lateinit var ID: ByteArray
-        lateinit var FLAGS: ByteArray
         var QR: Int? = null
         var opcode: Int? = null
         var AA: Int? = null
@@ -16,9 +36,66 @@ class Message {
         var RA: Int? = null
         var Z: Int? = null
         var RCODE: Int? = null
+
+        val FLAGS: ByteArray
+            get() {
+                val byte1 = (
+                        QR!! or
+                                opcode!! or
+                                AA!! or
+                                TC!! or
+                                RD!! or
+                                RA!!
+                        ).toByte()
+
+                val byte2 = (Z!! or RCODE!!).toByte()
+
+                return byteArrayOf(byte1, byte2)
+            }
+
+        var QDCOUNT: Int? = null
+        var ANCOUNT: Int? = null
+        var NSCOUNT: Int? = null
+        var ARCOUNT: Int? = null
+
+        val counts: ByteArray
+            get() {
+                val buf = ByteBuffer.allocate(8)
+                buf.putShort(QDCOUNT!!.toShort())
+                buf.putShort(ANCOUNT!!.toShort())
+                buf.putShort(NSCOUNT!!.toShort())
+                buf.putShort(ARCOUNT!!.toShort())
+
+                return buf.array()
+            }
+
+        val byteArray: ByteArray
+            get() {
+                val arrayList = ArrayList<Byte>()
+                arrayList.addAll(ID.toTypedArray())
+                arrayList.addAll(FLAGS.toTypedArray())
+                arrayList.addAll(counts.toTypedArray())
+
+                return arrayList.toByteArray()
+            }
+
+        /**
+        ## QDCOUNT: 无符号16bit整数表示报文请求段中的问题记录数
+        ## ANCOUNT: 无符号16bit整数表示报文回答段中的回答记录数
+        ## NSCOUNT: 无符号16bit整数表示报文授权段中的授权记录数
+        ## ARCOUNT: 无符号16bit整数表示报文附加段中的附加记录数
+         */
     }
 
     class Question {
+
+        /**
+        # Question
+        ## QNAME 无符号8bit为单位长度不限表示查询名(广泛的说就是：域名)
+        ## QTYPE 无符号16bit整数表示查询的协议类型
+        ## QCLASS 无符号16bit整数表示查询的类,比如，IN代表Internet
+         */
+
         lateinit var QNAME: String
         var QTYPE: Int? = null
         var QCLASS: Int? = null
@@ -38,5 +115,26 @@ class Message {
                 }
                 return arrayList.toByteArray()
             }
+
+        val byteArray: ByteArray
+            get() {
+                val arrayList = ArrayList<Byte>()
+
+                arrayList.addAll(QNAMEByteArray.toTypedArray())
+
+                val tmp = ByteArray(2)
+                val tmpByteBuffer = ByteBuffer.wrap(tmp)
+
+                tmpByteBuffer.putShort(QTYPE!!.toShort())
+                arrayList.addAll(tmp.toTypedArray())
+
+                tmpByteBuffer.clear()
+                tmpByteBuffer.putShort(QCLASS!!.toShort())
+                arrayList.addAll(tmp.toTypedArray())
+
+                return arrayList.toByteArray()
+            }
     }
+
+    class Answer
 }
