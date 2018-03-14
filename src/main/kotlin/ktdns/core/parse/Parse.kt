1ce0bbc9a3812ee775ac90fe20f01a4172e2e268
Sherlock Holo <sherlockya@gmail.com>
2018-03-14 22:10:39 +0800

    Clean codes

--- src/main/kotlin/ktdns/core/parse/Parse.kt
+++ src/main/kotlin/ktdns/core/parse/Parse.kt
@@ -9,86 +9,6 @@ import java.nio.ByteBuffer
 
 class Parse {
     fun parseQuery(buf: ByteArray, socket: DatagramSocket, sourceAddress: InetAddress, sourcePort: Int): Message {
-        /*val message = Message()
-
-        */
-        /**
-        # Header
-        ## ID: 2 bytes
-        ## FLAGS: 2 bytes
-        ### (包含 QR, opcode, AA, TC, RD, RA, Z, RCODE) {
-        QR: 0表示查询报文，1表示响应报文
-        opcode: 通常值为0（标准查询），其他值为1（反向查询）和2（服务器状态请求）,[3,15]保留值
-        AA: 表示授权回答（authoritative answer）– 这个比特位在应答的时候才有意义，指出给出应答的服务器是查询域名的授权解析服务器
-        TC: 表示可截断的（truncated）–用来指出报文比允许的长度还要长，导致被截断
-        RD: 表示期望递归(Recursion Desired) – 这个比特位被请求设置，应答的时候使用的相同的值返回。如果设置了RD，就建议域名服务器进行递归解析，递归查询的支持是可选的
-        RA: 表示支持递归(Recursion Available) – 这个比特位在应答中设置或取消，用来代表服务器是否支持递归查询
-        Z : 保留值，暂未使用
-        RCODE: 应答码(Response code) - 这4个比特位在应答报文中设置，代表的含义如下:
-        0: 没有错误
-        1: 报文格式错误
-        2: 服务器失败
-        3: 名字错误
-        4: 没有实现
-        5: 服务器因为策略而不予以应答
-        6-15: 未使用
-        }
-
-        ## QDCOUNT: 无符号16bit整数表示报文请求段中的问题记录数
-        ## ANCOUNT: 无符号16bit整数表示报文回答段中的回答记录数
-        ## NSCOUNT: 无符号16bit整数表示报文授权段中的授权记录数
-        ## ARCOUNT: 无符号16bit整数表示报文附加段中的附加记录数
-
-        0  1  2  3  4  5  6  7  0  1  2  3  4  5  6  7
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                      ID                       |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |QR|   opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                    QDCOUNT                    |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                    ANCOUNT                    |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                    NSCOUNT                    |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                    ARCOUNT                    |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-         *//*
-
-
-        val ID = buf.copyOfRange(0, 2)
-        val FLAGS = buf.copyOfRange(2, 4)
-        val QR = FLAGS[0].toUInt() and 0b10000000 ushr 7
-        val opcode = FLAGS[0].toUInt() and 0b01111000 ushr 3
-        val AA = FLAGS[0].toUInt() and 0b00000100 ushr 2
-        val TC = FLAGS[0].toUInt() and 0b00000010 ushr 1
-        val RD = FLAGS[0].toUInt() and 0b00000001
-        val RA = FLAGS[1].toUInt() and 0b10000000 ushr 7
-        val Z = FLAGS[1].toUInt() and 0b01110000 ushr 4
-        val RCODE = FLAGS[1].toUInt() and 0b00001111
-
-        val counts = ByteBuffer.wrap(buf.copyOfRange(4, 12))
-        val QDCOUNT = counts.short.toInt()
-        val ANCOUNT = counts.short.toInt()
-        val NSCOUNT = counts.short.toInt()
-        val ARCOUNT = counts.short.toInt()
-
-        val header = message.header
-
-        header.ID = ID
-        header.QR = QR
-        header.opcode = opcode
-        header.AA = AA
-        header.TC = TC
-        header.RD = RD
-        header.RA = RA
-        header.Z = Z
-        header.RCODE = RCODE
-        header.QDCOUNT = QDCOUNT
-        header.ANCOUNT = ANCOUNT
-        header.NSCOUNT = NSCOUNT
-        header.ARCOUNT = ARCOUNT*/
-
         val message = parseHeader(buf)
 
 
@@ -110,24 +30,9 @@ class Parse {
         ## QCLASS 无符号16bit整数表示查询的类,比如，IN代表Internet
          */
 
-
         val noHeaderBuf = buf.copyOfRange(12, buf.size)
-        val QNAMEBuilder = StringBuilder()
 
-        var pos = 0
-        var length = noHeaderBuf[pos].toUInt()
-
-        do {
-            pos++
-            QNAMEBuilder.append(String(noHeaderBuf.copyOfRange(pos, pos + length)))
-            QNAMEBuilder.append('.')
-
-            pos += length
-            length = noHeaderBuf[pos].toUInt()
-
-        } while (length != 0)
-
-        val QNAME = QNAMEBuilder.toString()
+        val QNAME = getName(0, noHeaderBuf).name
         val QTYPE = ByteBuffer.wrap(noHeaderBuf.copyOfRange(QNAME.length + 1, QNAME.length + 3)).short.toInt()
         val QCLASS = ByteBuffer.wrap(noHeaderBuf.copyOfRange(QNAME.length + 3, QNAME.length + 5)).short.toInt()
 
@@ -146,86 +51,6 @@ class Parse {
     }
 
     fun parseAnswer(buf: ByteArray): Message {
-        /*val message = Message()
-
-        */
-        /**
-        # Header
-        ## ID: 2 bytes
-        ## FLAGS: 2 bytes
-        ### (包含 QR, opcode, AA, TC, RD, RA, Z, RCODE) {
-        QR: 0表示查询报文，1表示响应报文
-        opcode: 通常值为0（标准查询），其他值为1（反向查询）和2（服务器状态请求）,[3,15]保留值
-        AA: 表示授权回答（authoritative answer）– 这个比特位在应答的时候才有意义，指出给出应答的服务器是查询域名的授权解析服务器
-        TC: 表示可截断的（truncated）–用来指出报文比允许的长度还要长，导致被截断
-        RD: 表示期望递归(Recursion Desired) – 这个比特位被请求设置，应答的时候使用的相同的值返回。如果设置了RD，就建议域名服务器进行递归解析，递归查询的支持是可选的
-        RA: 表示支持递归(Recursion Available) – 这个比特位在应答中设置或取消，用来代表服务器是否支持递归查询
-        Z : 保留值，暂未使用
-        RCODE: 应答码(Response code) - 这4个比特位在应答报文中设置，代表的含义如下:
-        0: 没有错误
-        1: 报文格式错误
-        2: 服务器失败
-        3: 名字错误
-        4: 没有实现
-        5: 服务器因为策略而不予以应答
-        6-15: 未使用
-        }
-
-        ## QDCOUNT: 无符号16bit整数表示报文请求段中的问题记录数
-        ## ANCOUNT: 无符号16bit整数表示报文回答段中的回答记录数
-        ## NSCOUNT: 无符号16bit整数表示报文授权段中的授权记录数
-        ## ARCOUNT: 无符号16bit整数表示报文附加段中的附加记录数
-
-        0  1  2  3  4  5  6  7  0  1  2  3  4  5  6  7
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                      ID                       |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |QR|   opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                    QDCOUNT                    |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                    ANCOUNT                    |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                    NSCOUNT                    |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        |                    ARCOUNT                    |
-        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-         *//*
-
-
-        val ID = buf.copyOfRange(0, 2)
-        val FLAGS = buf.copyOfRange(2, 4)
-        val QR = FLAGS[0].toUInt() and 0b10000000 ushr 7
-        val opcode = FLAGS[0].toUInt() and 0b01111000 ushr 3
-        val AA = FLAGS[0].toUInt() and 0b00000100 ushr 2
-        val TC = FLAGS[0].toUInt() and 0b00000010 ushr 1
-        val RD = FLAGS[0].toUInt() and 0b00000001
-        val RA = FLAGS[1].toUInt() and 0b10000000 ushr 7
-        val Z = FLAGS[1].toUInt() and 0b01110000 ushr 4
-        val RCODE = FLAGS[1].toUInt() and 0b00001111
-
-        val counts = ByteBuffer.wrap(buf.copyOfRange(4, 12))
-        val QDCOUNT = counts.short.toInt()
-        val ANCOUNT = counts.short.toInt()
-        val NSCOUNT = counts.short.toInt()
-        val ARCOUNT = counts.short.toInt()
-
-        val header = message.header
-
-        header.ID = ID
-        header.QR = QR
-        header.opcode = opcode
-        header.AA = AA
-        header.TC = TC
-        header.RD = RD
-        header.RA = RA
-        header.Z = Z
-        header.RCODE = RCODE
-        header.QDCOUNT = QDCOUNT
-        header.ANCOUNT = ANCOUNT
-        header.NSCOUNT = NSCOUNT
-        header.ARCOUNT = ARCOUNT*/
-
         val message = parseHeader(buf)
 
 
@@ -247,24 +72,8 @@ class Parse {
         ## QCLASS 无符号16bit整数表示查询的类,比如，IN代表Internet
          */
 
-
         val noHeaderBuf = buf.copyOfRange(12, buf.size)
-        /*val QNAMEBuilder = StringBuilder()
 
-        var pos = 0
-        var length = noHeaderBuf[pos].toUInt()
-
-        do {
-            pos++
-            QNAMEBuilder.append(String(noHeaderBuf.copyOfRange(pos, pos + length)))
-            QNAMEBuilder.append('.')
-
-            pos += length
-            length = noHeaderBuf[pos].toUInt()
-
-        } while (length != 0)*/
-
-//        val QNAME = QNAMEBuilder.toString()
         val QNAME = getName(0, noHeaderBuf).name
         val QTYPE = ByteBuffer.wrap(noHeaderBuf.copyOfRange(QNAME.length + 1, QNAME.length + 3)).short.toInt()
         val QCLASS = ByteBuffer.wrap(noHeaderBuf.copyOfRange(QNAME.length + 3, QNAME.length + 5)).short.toInt()
@@ -316,8 +125,6 @@ class Parse {
             val `class` = (BytesNumber.getNumber(buf.copyOfRange(newPos + 2, newPos + 4)) as Short).toInt()
             val ttl = BytesNumber.getNumber(buf.copyOfRange(newPos + 4, newPos + 8)) as Int
             val rdlength = (BytesNumber.getNumber(buf.copyOfRange(newPos + 8, newPos + 10)) as Short).toInt()
-
-//            newPos += 2 + 2 + 2 + 4 + 2
 
             newPos += 2 + 2 + 4 + 2
 
@@ -427,7 +234,6 @@ class Parse {
         |                    ARCOUNT                    |
         +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
          */
-
 
         val ID = buf.copyOfRange(0, 2)
         val FLAGS = buf.copyOfRange(2, 4)
--- src/test/kotlin/ktdns/server/SimpleResolverTest.kt
+++ src/test/kotlin/ktdns/server/SimpleResolverTest.kt
@@ -4,5 +4,5 @@ import java.net.InetSocketAddress
 
 fun main(args: Array<String>) {
     val simpleResolver = SimpleResolver(InetSocketAddress("127.0.0.1", 53), InetSocketAddress(5454))
-    simpleResolver.start()                                                          
+    simpleResolver.start()
 }
