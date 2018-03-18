51fcf6f5be8b67fb84de97717b6b460ed770ede9
Sherlock Holo <sherlockya@gmail.com>
2018-03-18 16:21:28 +0800

    Delete useless codes and add String.times extend function

--- src/main/kotlin/ktdns/extend/Extend.kt
+++ src/main/kotlin/ktdns/extend/Extend.kt
@@ -4,20 +4,6 @@ import java.nio.ByteBuffer
 
 fun Byte.toUInt() = this.toInt() and 0xff
 
-fun ByteBuffer.getShortByteArray(short: Short): ByteArray {
-    if (this.capacity() != 2) throw IllegalStateException("byteBuffer size is ${this.capacity()}")
-    this.clear()
-    this.putShort(short)
-    return this.array()
-}
-
-fun ByteBuffer.getIntByteArray(int: Int): ByteArray {
-    if (this.capacity() != 4) throw IllegalStateException("byteBuffer size is ${this.capacity()}")
-    this.clear()
-    this.putInt(int)
-    return this.array()
-}
-
 class BytesNumber {
     companion object {
         private val intBuffer = ByteBuffer.allocate(4)
@@ -77,4 +63,12 @@ class BytesNumber {
             }
         }
     }
+}
+
+operator fun String.times(times: Int): String {
+    val sb = StringBuilder()
+    for (i in 0 until times) {
+        sb.append(this)
+    }
+    return sb.toString()
 }
